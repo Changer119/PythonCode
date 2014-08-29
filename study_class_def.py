78c6e4f6d7f0ffaecb6a2c85d361c21fc95f23fd
Changer119 <fachang.jiang@gmail.com>
2014-08-29 22:04:31 +0800

    Add some python shell

---
+++ study_class_def.py
@@ -0,0 +1,11 @@
+# !/usr/bin/python
+# -*- coding: <utf-8> -*-
+
+class Student(object):
+	def __init__(self, name, age):
+		self.name = name
+		self.age =age
+
+stu = Student('fcjiang', 28)
+print stu
+
